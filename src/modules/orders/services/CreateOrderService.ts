6df9a35b2130e17b9ee3a906b0766e71c84b735a
Allyson Barros <allysonbarrosrn@gmail.com>
2020-07-16 09:45:17 -0300

    Passando 10 de 10 testes.

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -79,6 +79,17 @@ class CreateOrderService {
       products: serializedProducts,
     });
 
+    const { order_products } = order;
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        findProductsByIDs.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
     return order;
   }
 }
